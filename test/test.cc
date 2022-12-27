496f96cfa1450b1c19e3394307a7733cd235fb73
ait <dauku@hotmail.com>
2022-12-27 20:39:50 +0600

    Add util positive test.

--- test/test.cc
+++ test/test.cc
@@ -10,7 +10,17 @@ TEST(Version, Positive) {
 }
 
 TEST(Util, Positive) {
-    std::uint8_t buffer[1024] { 0 };
-    auto rc = zeroize(buffer, sizeof(buffer));
+    std::uint8_t buff[128] { 0 };
+    auto rc = random(buff, sizeof(buff));
     ASSERT_EQ(rc, RC::OK);
+    std::size_t i = 0;
+    for (; i < sizeof(buff); ++i) if (buff[i]) break;
+    ASSERT_NE(i, sizeof(buff));
+    printArray("Buffer", buff, sizeof(buff), true);
+    rc = zeroize(buff, sizeof(buff));
+    ASSERT_EQ(rc, RC::OK);
+    i = 0;
+    for (; i < sizeof(buff); ++i) if (buff[i]) break;
+    ASSERT_EQ(i, sizeof(buff));
+    printArray("Buffer", buff, sizeof(buff), true);
 }
