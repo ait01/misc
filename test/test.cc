0b8d7cfe671bb1819321f690206d17ba9c9ec819
ait <dauku@hotmail.com>
2022-12-27 16:13:27 +0600

    Add unit tests.

--- test/test.cc
+++ test/test.cc
@@ -1,8 +1,16 @@
 #include <iostream>
 #include <gtest/gtest.h>
 #include "version.hh"
+#include "util.hh"
+#include "log.hh"
 
 TEST(Version, Positive) {
     std::cout << "Version " << VERSION << std::endl;
     std::cout << "Commit " << COMMIT << std::endl;
+}
+
+TEST(Util, Positive) {
+    std::uint8_t buffer[1024] { 0 };
+    auto rc = zeroize(buffer, sizeof(buffer));
+    ASSERT_EQ(rc, RC::OK);
 }
