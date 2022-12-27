90b8bfb367fb829414249a478d1920e104dc188d
ait <dauku@hotmail.com>
2022-12-27 20:39:07 +0600

    Add random.

--- include/util.hh
+++ include/util.hh
@@ -2,8 +2,12 @@
 #define UTIL_H
 
 #include <cstdlib>
+#include <cstdint>
 #include "rc.hh"
 
 RC zeroize(void *s, std::size_t n);
+RC random(std::uint8_t *buff, std::size_t buffLen);
+RC random(std::vector<std::uint8_t> &buff);
+std::vector<std::uint8_t> random(std::size_t len);
 
 #endif //UTIL_H
--- util/util.cc
+++ util/util.cc
@@ -1,4 +1,4 @@
-#include <cstdint>
+#include <random>
 #include "util.hh"
 #include "log.hh"
 
@@ -16,5 +16,40 @@ RC zeroize(void *s, std::size_t n)
     volatile std::uint8_t *p = (std::uint8_t*)s;
     while (n--) *p++ = 0;
     LOGD("Memory zeroized.");
+    return RC::OK;
+}
+
+std::vector<std::uint8_t> random(std::size_t len)
+{
+    std::vector<std::uint8_t> buff;
+    std::random_device r;
+    std::default_random_engine e(r());
+    std::uniform_int_distribution<std::uint8_t> u(1);
+    for (std::size_t i = 0; i < len; ++i) buff.push_back(u(e));
+    return buff;
+}
+
+RC random(std::vector<std::uint8_t> &buff)
+{
+    if (buff.empty()) {
+        LOGE("Buffer is empty.");
+        return RC::BAD_PARAMS;
+    }
+    buff = random(buff.size());
+    return RC::OK;
+}
+
+RC random(std::uint8_t *buff, std::size_t buffLen)
+{
+    if (!buffLen) {
+        LOGE("Bad parameter: buffer length is 0.");
+        return RC::BAD_PARAMS;
+    }
+    if (buff == nullptr) {
+        LOGE("Bad parameter: buffer is nullptr.");
+        return RC::BAD_PARAMS;
+    }
+    auto buff_ = random(buffLen);
+    std::copy(buff_.cbegin(), buff_.cend(), buff);
     return RC::OK;
 }
